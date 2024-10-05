ca0784213db0f1ffdfae9e4d7e01dc603e26556b
AO™ <anthony@osawere.com>
2024-10-05 22:01:15 +0100

    Create HttpX.php

---
+++ Anci/HttpX.php
@@ -0,0 +1,8 @@
+<?php //*** HttpX ~ class » Yaic™ Library for Laravel © 2024 ∞ AO™ • @osawereao • www.osawere.com ∞ Apache License ***//
+
+namespace App\Yaic\Anci;
+
+class HttpX
+{
+
+}//> end of class ~ HttpX
