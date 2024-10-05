5fb1880db6a42cf104b58e1f3cb8d681125d53c2
AO™ <anthony@osawere.com>
2024-10-05 21:55:07 +0100

    Create BladeX.php

---
+++ Anci/BladeX.php
@@ -0,0 +1,18 @@
+<?php //*** BladeX ~ class » Yaic™ Library for Laravel © 2024 ∞ AO™ • @osawereao • www.osawere.com ∞ Apache License ***//
+
+namespace App\Yaic\Anci;
+
+class BladeX
+{
+	// ◈ property
+	public static $yaic;
+
+
+
+
+	// ◈ === method »
+	public static function method($var)
+	{
+	}
+
+}//> end of class ~ BladeX
