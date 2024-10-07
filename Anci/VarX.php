d8ad95001fa0f65ebb2eaf8cd7da07da3e920eb5
AO™ <anthony@osawere.com>
2024-10-07 23:15:43 +0100

    Update VarX.php

--- Anci/VarX.php
+++ Anci/VarX.php
@@ -4,6 +4,27 @@ namespace App\Yaic\Anci;
 
 class VarX
 {
+	// ◈ === is » boolean
+	public static function is(&$var = null, $comparison = null, $strickCheck = false)
+	{
+		if (is_null($var)) {
+			return false;
+		}
+		if (is_null($comparison)) {
+			if ($var === 0) {
+				return true;
+			}
+			if (!empty($var)) {
+				return true;
+			}
+		}
+		// TODO: implement code for comparison & strick check
+		return false;
+	}
+
+
+
+
 
 	// ◈ === safe »
 	public static function safe(&$var = null, $default = '')
